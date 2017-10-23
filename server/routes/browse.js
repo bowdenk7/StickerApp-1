2450bc42c01ae81b1c19bf76a122f0e372a5d95a
Bowden Kelly <bossman@Bowdens-MacBook-Pro.local>
2017-10-22 20:13:39 -0700

    added some basic demo stuff

--- server/routes/browse.js
+++ server/routes/browse.js
@@ -5,10 +5,38 @@ const router = new express.Router();
 router.use(bodyParser.json());
 
 router.get('/', function stickerRouteBrowse(req, res) {
-    const renderData = { pageTitle: 'Browse', entry: 'browse' };
-    console.log('Render values: ', renderData);
+    //console.log('Render values: ', renderData);
+    
+    const renderData = { pageTitle: 'Browse', entry: 'browse', cookedSentiment: getSentimentAnalysis() };
     res.render('index', renderData);
 });
+
+function getSentimentAnalysis() {
+    // Demo code below
+    // query to pull tweets + sentiment from graph
+    let rawSentiment = [
+        {
+            text: 'I love #hotel360!!!',
+            score: 9
+        },
+        {
+            text: '#hotel360 is the worst!',
+            score: 2
+        }
+    ];
+
+    // we need to add an emotion to each tweet so we know which icon to use on client
+    let cookedSentiment = [];
+    for (let tweet of rawSentiment) {
+        if (tweet.score > 7) {
+            tweet.emotion = 'happy';
+        } else {
+            tweet.emotion = 'sad';
+        }
+        cookedSentiment.push(tweet);
+    }
+    return cookedSentiment;
+}
 
 const db = require('../db');
 router.get('/api/items', function stickerRouteApiBrowse(req, res) {
